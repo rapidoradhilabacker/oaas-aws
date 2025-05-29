b6e23b5a7b51f9a3c19806f0551aa5b8f448286b
rapidoradhilabacker <adhil.backer@acelrtech.com>
2025-05-29 07:36:21 +0000

    feat: implement JWT token generation endpoint with service authentication

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@
 
 from fastapi import FastAPI
 from app.routers import router
+from app.auth_api import auth_router
 
 app = FastAPI(title="FastAPI AWS S3 Service")
 
 # Include S3 routes at the `/s3` prefix.
 app.include_router(router, prefix="/s3", tags=["S3 File Service"])
+
+# Include Auth routes at the `/auth` prefix
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 
 @app.get("/")
 async def read_root():
